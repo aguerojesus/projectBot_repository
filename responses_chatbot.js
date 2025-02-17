35196d9358dc6f650cf020ca72efa49040488282
AlexMauri <alex.urena@ucr.ac.cr>
2025-02-17 13:52:02 -0600

    Se añaden más respuestas y se añaden los contactos del recinto

--- responses_chatbot.js
+++ responses_chatbot.js
@@ -1,14 +1,170 @@
 const respuestas = {
-    Concepto_Prematricula: "La prematrícula es el proceso en el que seleccionas cursos antes de la matrícula oficial.",
-    Proceso_Prematricula: "Para realizar la prematrícula, ingresa al sistema, elige tus cursos y confirma.",
+    Categoria_Beca: "Las becas se clasifican en: Becas socioeconómica, beca por actividades universitarias y becas externas",
+    Categoria_Horas: "Algunas becas o programas requieren que los estudiantes cumplan con un número específico de horas en actividades extracurriculares o de servicio.",
+    Concepto_Beca: "El concepto de beca se refiere a su propósito, que es apoyar a los estudiantes en su formación académica.",
     Concepto_Matricula: "La matrícula es el proceso oficial donde inscribes los cursos para el semestre.",
+    Concepto_Prematricula: "La prematrícula es el proceso en el que seleccionas cursos antes de la matrícula oficial.",
+    Concepto_ProfesorConsejero: "Un profesor consejero es el encargado de velar por la orientación académica de los estudiantes, ayudándoles en la selección de cursos y resolución de problemas académicos.",
+    Consejo_Matricula: "El consejo de matrícula se refiere a la orientación que recibe un estudiante para seleccionar sus cursos de manera adecuada.",
+    Consejo_Prematricula: "El consejo de prematrícula es la guía que recibe un estudiante para planificar sus cursos antes de la matrícula oficial.",
+    Fecha_Matricula: "Las fechas de prematrícula y matrícula son importantes para que los estudiantes se organicen y cumplan con los plazos establecidos.",
+    Proceso_Becas: "El proceso de becas incluye la presentación de documentos, cumplimiento de requisitos y evaluación por parte de la institución. Puedes consultar los pasos, documentos necesarios y las fechas en el siguiente link: <a href='https://becas.ucr.ac.cr/' target='_blank'>Proceso de Becas UCR</a>.",
     Proceso_Matricula: "Para matricularte, entra al sistema en las fechas oficiales y selecciona los cursos.",
+    Proceso_Prematricula: "Para realizar la prematrícula, ingresa al sistema de Ematricula, selecciona un curso en el que estés interesado en prematricular y selecciona la bolita de la izquierda, luego selecciona el horario en el que prefieras prematricular(mañana-tarde-noche) y por último le das a la opción de agregar que está en la zona superior de los cursos; repite el mismo proceso con cada curso que quieras prematricular, al finalizar, envía tu prematricula para que tu profesor consejero lo acepte; recuerda estar pendiente a la revisión de esta, y aceptar dicha revisión",
+    Proceso_Admision: "El proceso de admisión en la UCR incluye los siguientes pasos: inscripción a la PAA, presentación de documentos, selección de carreras y espera de los resultados. Asegúrate de revisar las fechas clave en el sitio web de la UCR.",
+    Solicitud_Carnet: "Para solicitar o renovar tu carnet universitario, debes presentar tu identificación y una foto reciente en la oficina de Registro. También puedes seguir el proceso en línea a través del sistema de la UCR.",
+    Solicitud_Graduacion: "Para solicitar tu graduación, debes cumplir con todos los requisitos académicos de tu carrera, presentar la solicitud en la oficina de Registro y pagar las tasas correspondientes. Asegúrate de revisar las fechas límite para no perder el período de graduación.",
+    Requisito_Curso: "Algunos cursos requieren que hayas aprobado asignaturas previas o que cumplas con un nivel académico específico dentro de la carrera. Revisa el plan de estudios de tu carrera para conocer los requisitos de cada curso.",
+    Requisito_Horas: "Algunos programas de horas requieren que completes un número mínimo de horas en actividades como trabajo comunitario, investigación o servicio social. Revisa los requisitos específicos del programa al que aplicas.",
+    
+    //Agregar a wit:
+    Categoria_Horas: "Existen 3 tipos de horas distintos: horas estudiante, horas asistente y horas asistente de posgrado",
+    //Revisar esto antes de meterlo a wit, ver como manejarlo:
+    Concepto_HorasEstudiante: "Las horas estudiante tienen carácter de colaboración y demandarán habilidades y conocimientos básicos para llevarlas a cabo. Para su ejecución, se requerirá la supervisión del personal universitario.",
+    Concepto_HorasAsistente: "Las horas asistente tienen carácter de colaboración, pero requieren de la población estudiantil mayor conocimiento, destrezas y responsabilidad",
+    Concepto_HorasAsistentePosgrado:"Las horas asistente de posgrado requieren conocimientos y habilidades propios de este nivel de estudios.",
+    Actividades_HorasEstudiante:"Preparar materiales que sirvan de apoyo en las diferentes actividades universitarias. Colaborar, bajo una supervisión,coordinada del personal académico,docente, en la corrección de tareas,pruebas, trabajos cortos, elaboración de material didáctico y otras actividades conexas. Apoyar al personal académico docente, según se considere pertinente, en actividades relacionadas con las lecciones y los procesos de evaluación de los cursos.  ",
+    Actividades_HorasAsistente:"Colaborar, bajo una supervisión coordinada del personal académico docente, en la corrección de tareas, pruebas, trabajos cortos, elaboración de material didáctico y otras actividades semejantes. Colaborar en la organización desarrollo y seguimiento de proyectos en los diferentes ámbitos del quehacer laboratorios o talleres. Colaborar en la organización, desarrollo y seguimiento de proyectos en los diferentes ámbitos del quehacer universitario. ",
+    Actividades_HorasAsistentePosgrado:"Apoyar en la preparación de informes,presentaciones y publicaciones. Colaborar en el diseño y aplicación de diversas metodologías y técnicas de investigación. Colaborar en la organización,ejecución y logística de proyectos en diferentes ámbitos del quehacer universitario.  Participar en actividades de un curso con la guía docente.",
+    Concepto_PlanDeEstudio: "Es el conjunto de exigencias curriculares que debe cumplir un estudiante para obtener un título o grado académico. El plan de estudios establece: Las horas de estudio por asignatura y por la carrera mediante créditos, Esto significa que para asignar el número de créditos de un curso, se debe establecer cuál será la carga académica semanal promedio para cada estudiante y los cursos que debe llevar, además de los requisitos y correquisitos de estos.",
+    Concepto_MallaCurricular:"Es el conjunto de asignaturas que un estudiante debe tomar para obtener su título universitario, estas suelen estar divididas en 'ramas' que indican el orden de los cursos a seguir por áreas de conocimiento.",
+    Concepto_CorteIngreso: "Es el puntaje mínimo requerido para ser admitido en esa carrera, sin embargo, no es un número fijo. Este corte de ingreso suele ser la nota de admisión de la última persona que fue seleccionada para ingresar en esa carrera, por lo que los cortes cambian año a año. I m p o r t a n t e: Algunas carreras tienen requisitos especiales como pruebas específicas o de habilidades se RECOMIENDA consultar las normas y procedimientos de ingreso a carrera, correspondiente al año de concurso. Puedes consultar los cortes de carrera en la siguiente dirección: <a href='https://consultasic.ucr.ac.cr/cortespi/' target='_blank'>Cortes UCR</a>.",
+    Concepto_PAA: "La Prueba de Aptitud Académica mide habilidades generales de razonamiento mediante ítems que evalúan el nivel de razonamiento de la persona aspirante, combinando conocimientos básicos de los primeros años de secundaria con estrategias como suponer, presuponer, parafrasear, oponer, deducir, reducir, generalizar, verificar, indagar y representar información. La Prueba del 2024 en total posee 50 ítems. Todos son de selección única con 4 opciones de respuesta, de las cuales solo una es correcta. Puedes practicar en línea en el siguiente enlace: <a href='https://www.paa.iip.ucr.ac.cr/practicaenlinea/' target='_blank'>Práctica en Línea PAA</a>.",
+   
+
+    Contacto_RecintoParaíso:"Teléfono:  2511-7550, Correo:secretaria.rp@ucr.ac.cr",
     por_defecto: "Lo siento, no entiendo tu pregunta. ¿Puedes ser más específico?"
 };
+
+
+const contacts = [
+    { name: "Abigail Velázquez Porras", email: "abigail.velasquez@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Adriana Morales Hernández", email: "adriana.moraleshernandez@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Alberto Acevedo Gutiérrez", email: "alberto.acevedo@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Alejandro Jiménez Tassara", email: "alejandro.jimenez@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Alexa Bolaños Carpio", email: "alexa.bolanos@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Alizon Calderón Torres", email: "alizon.calderon@ucr.ac.cr", phone: "2511-7557" },
+    { name: "Allan Castro Prieto", email: "allan.castro@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Allan Ramírez Cerdas", email: "allan.ramirezcerdas@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Álvaro Mena Monge", email: "alvaro.mena@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Ana Catalina Camacho Navarro", email: "anacatalina.camacho@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Ana Catalina Siles Solano", email: "ana.siles@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Ana Felicia Solano Arguedas", email: "anafelicia.solano@ucr.ac.cr", phone: "2511-7580" },
+    { name: "Ana Lucia Coto Chinchilla", email: "ana.coto@ucr.ac.cr", phone: "2511-7560" },
+    { name: "Ana Moya Monge", email: "ana.moya@ucr.ac.cr", phone: "2511-7559" },
+    { name: "Andrea Ramírez Zúniga", email: "andrea.ramirezzuniga@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Andrés Jiménez Fernández", email: "andres.jimenez17@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Andrés Meseguer Rojas", email: "andres.messeguer@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Auxiliadora Vega Bustos", email: "maria.vegabustos@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Bianca Paz García", email: "bianca.paz@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Carolina Brenes Molina", email: "carolina.brenesmolina@ucr.ac.cr", phone: "2511-7556" },
+    { name: "Caseta de seguridad", email: "seguridad.rp@ucr.ac.cr", phone: "(506) 2511-7565" },
+    { name: "César Fonseca Araya", email: "cesar.fonsecaaraya@ucr.ac.cr", phone: "2511-7565" },
+    { name: "Cinthya Arce Alvarado", email: "cinthya.arcealvarado@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Daniel Flores Gómez", email: "daniel.floresgomez@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Edgar Rojas Quesada", email: "edgar.rojasquesada@ucr.ac.cr", phone: "2511-7565" },
+    { name: "Edgardo Araya Sibaja", email: "edgardo.araya@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Elvia Ureña Salazar", email: "elvia.urena@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Ernesto Valverde Villalobos", email: "e.valverde.villalobos@gmail.com", phone: "2511-7550" },
+    { name: "Esteban Sanabria Mora", email: "esteban.sanabria@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Evelyn Fernández Fonseca", email: "evelyn.fernandez@ucr.ac.cr", phone: "2511-7568" },
+    { name: "Ezequiel Solano Corrales", email: "ezequiel.solano@ucr.ac.cr", phone: "2511-7558" },
+    { name: "Fabio Alvarado Arce", email: "fabio.alvaradoarce@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Federico Rivera Romero", email: "federico.rivera@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Fernando Camacho Mora", email: "fernando.camachomora@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Gabriela Guevara Mora", email: "gabriela.guevara@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Geovanni Figueroa Mata", email: "geovanny.figueroa@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Gilberth Chaves Ávila", email: "gilberth.chavesavila@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Gina Arrieta Molina", email: "gina.arrieta@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Guillermo González Campos", email: "guillermo.gonzalezcampos@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Gustavo Romero Zúñiga", email: "gustavo.romero@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Hannia León Fuentes", email: "hannia.leon_f@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Henry Arce Ramírez", email: "henry.arce@ucr.ac.cr", phone: "2511-7550" },
+    { name: "José David Ulloa Soto", email: "jose.ulloasoto@ucr.ac.cr", phone: "2511-7550" },
+    { name: "José Eduardo Gutiérrez Durán", email: "jose.gutierrezduran@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Jose Francisco Acevedo", email: "jose.acevedogutierrez@ucr.ac.cr", phone: "2511-7550" },
+    { name: "José Luis Vásquez Vásquez", email: "jose.vasquez@ucr.ac.cr", phone: "2511-7550" },
+    { name: "José Paz Barahona", email: "jose.paz@ucr.ac.cr", phone: "2511-7550" },
+    { name: "José Rafael Sánchez Meza", email: "rafael.sanchezmeza@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Juan Barahona Cubillo", email: "juan.barahona@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Karen Vindas Monestel", email: "karen.vindasmonestel@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Kenneth Carvajal Maykall", email: "kenneth.carvajal@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Leonardo Astorga Sanchez", email: "leonardo.astorgasanchez@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Livieth Gamboa Solano", email: "livieth.gamboa@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Lucia Rescia Chinchilla", email: "ana.rescia@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Luis González Martínez", email: "luis.gonzalezmartinez@ucr.ac.cr", phone: "2511-7565" },
+    { name: "Madelyn Vanesa Vives Rivera", email: "madelyn.vives@ucr.ac.cr", phone: "2511-7563" },
+    { name: "Mainor Aguilar Moya", email: "mainor.aguilarmoya@ucr.ac.cr", phone: "2511-7564" },
+    { name: "Manuel Arce Sáenz", email: "manuel.arce@ucr.ac.cr", phone: "2511-7550" },
+    { name: "María Garita Sánchez", email: "mariadelrosario.garita@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Marisol Mayorga Castro", email: "marisol.mayorga@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Maritza Veich Forbes", email: "marixa.veitch@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Mauricio Chinchilla Bonilla", email: "mauricio.chinchilla@ucr.ac.cr", phone: "2511-7566" },
+    { name: "Milena Barquero D’Avanzo", email: "milena.barquero@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Nancy Pérez Valverde", email: "nancy.perezvalverde@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Nathalia María Monestel Velásquez", email: "nathalia.monestel@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Nathalia Solis Pérez", email: "natalia.solisperez@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Nelson Moya Moya", email: "nelson.moya@ucr.ac.cr", phone: "2511-7552" },
+    { name: "Nicole Velásquez Acuña", email: "nicole.velazquez@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Nuria Solano Coto", email: "nuria.solano@ucr.ac.cr", phone: "2511-7561" },
+    { name: "Olga Medina Ruiz", email: "", phone: "2511-7550" },
+    { name: "Patricia Delvó Gutiérrez", email: "patricia.delvo@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Paula Moya Castillo", email: "paula.moya@ucr.ac.cr", phone: "2511-7574" },
+    { name: "Percival Kelso Aguilar Segura", email: "percival.aguilar@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Raimy Camacho Ramírez", email: "raymi.camacho@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Randall Jiménez Mora", email: "randall.jimenez@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Raquel Delgado Solano", email: "raquel.delgado@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Renato Sánchez Mena", email: "renato.sanchez@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Ricardo Sánchez Rodríguez", email: "ricardo.sanchez@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Rocío Delgado González", email: "rocio.delgado@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Rodrigo Gamboa Solano", email: "rodrigo.gamboa@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Roxana González Castro", email: "roxana.gonzalez@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Ruth Paniagua León", email: "ruth.paniagua@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Saúl Vargas Vargas", email: "saul.vargas@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Sebastián Morales Rodríguez", email: "sebastian.morales@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Silvia Mayorga Arias", email: "silvia.mayorga@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Sofía Quesada Soto", email: "sofia.quesada@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Sonia Barrantes Quesada", email: "sonia.barrantes@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Tania Castro Morera", email: "tania.castro@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Tatiana Vega Porras", email: "tatiana.vega@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Virginia Ortiz González", email: "virginia.ortiz@ucr.ac.cr", phone: "2511-7550" },
+    { name: "Yanelki Rodríguez Sánchez", email: "yanelki.rodriguez@ucr.ac.cr", phone: "2511-7550" },
+  ];
+  
+
+//MATRICULA
+//PREMATRICULA
+//UBICACIÓN DE LA U
+//CONTACTOS DE LA U
+//PROCESOS DE BECA
+//CALENDARIO
+
+
 
 // Función para obtener una respuesta según la intención detectada
 function obtenerRespuesta(intent) {
     return respuestas[intent] || respuestas["por_defecto"];
 }
 
-module.exports = { obtenerRespuesta };
+
+// Función para responder con los contactos
+function obtenerContactosPorCriterio(criterio) {
+    if (!criterio || typeof criterio !== 'string') {
+      return "Por favor, ingresa un criterio de búsqueda válido";
+    }
+  
+    const criterioLower = criterio.toLowerCase();
+  
+    const contactosEncontrados = contacts.filter(contacto => {
+      // Se comprueba que la propiedad "name" exista y se realiza la búsqueda insensible a mayúsculas/minúsculas
+      return contacto.name && contacto.name.toLowerCase().includes(criterioLower);
+    });
+  
+    return contactosEncontrados.length > 0 ? contactosEncontrados : "No se encontraron contactos";
+  }
+
+// Solo funciona con nombres completos
+console.log(obtenerContactosPorCriterio("José "));
+
+
+module.exports = { obtenerRespuesta,  obtenerContactosPorCriterio};
