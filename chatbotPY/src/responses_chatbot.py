41d1e5f717b59b1fd310aab39a44f04670046587
AlexMauri <alex.urena@ucr.ac.cr>
2025-02-20 19:25:36 -0600

    se añaden intents de respuesta del bot

--- chatbotPY/src/responses_chatbot.py
+++ chatbotPY/src/responses_chatbot.py
@@ -16,36 +16,42 @@ respuestas = {
     "Solicitud_Graduacion": "Para solicitar tu graduación, debes cumplir con todos los requisitos académicos de tu carrera, presentar la solicitud en la oficina de Registro y pagar las tasas correspondientes. Asegúrate de revisar las fechas límite para no perder el período de graduación.",
     "Requisito_Curso": "Algunos cursos requieren que hayas aprobado asignaturas previas o que cumplas con un nivel académico específico dentro de la carrera. Revisa el plan de estudios de tu carrera para conocer los requisitos de cada curso.",
     "Requisito_Horas": "Algunos programas de horas requieren que completes un número mínimo de horas en actividades como trabajo comunitario, investigación o servicio social. Revisa los requisitos específicos del programa al que aplicas.",
-    
-    
-    #FALTA METER A WIT:
     "Categoria_Horas": "Existen 3 tipos de horas distintos: horas estudiante, horas asistente y horas asistente de posgrado",
     "Concepto_HorasEstudiante": "Las horas estudiante tienen carácter de colaboración y demandarán habilidades y conocimientos básicos para llevarlas a cabo. Para su ejecución, se requerirá la supervisión del personal universitario.",
     "Concepto_HorasAsistente": "Las horas asistente tienen carácter de colaboración, pero requieren de la población estudiantil mayor conocimiento, destrezas y responsabilidad",
     "Concepto_HorasAsistentePosgrado": "Las horas asistente de posgrado requieren conocimientos y habilidades propios de este nivel de estudios.",
-    "Actividades_HorasEstudiante": "Preparar materiales que sirvan de apoyo en las diferentes actividades universitarias. Colaborar, bajo una supervisión, coordinada del personal académico, docente, en la corrección de tareas, pruebas, trabajos cortos, elaboración de material didáctico y otras actividades conexas. Apoyar al personal académico docente, según se considere pertinente, en actividades relacionadas con las lecciones y los procesos de evaluación de los cursos.",
-    "Actividades_HorasAsistente": "Colaborar, bajo una supervisión coordinada del personal académico docente, en la corrección de tareas, pruebas, trabajos cortos, elaboración de material didáctico y otras actividades semejantes. Colaborar en la organización desarrollo y seguimiento de proyectos en los diferentes ámbitos del quehacer laboratorios o talleres. Colaborar en la organización, desarrollo y seguimiento de proyectos en los diferentes ámbitos del quehacer universitario.",
-    "Actividades_HorasAsistentePosgrado": "Apoyar en la preparación de informes, presentaciones y publicaciones. Colaborar en el diseño y aplicación de diversas metodologías y técnicas de investigación. Colaborar en la organización, ejecución y logística de proyectos en diferentes ámbitos del quehacer universitario. Participar en actividades de un curso con la guía docente.",
     "Concepto_PlanDeEstudio": "Es el conjunto de exigencias curriculares que debe cumplir un estudiante para obtener un título o grado académico. El plan de estudios establece: Las horas de estudio por asignatura y por la carrera mediante créditos, Esto significa que para asignar el número de créditos de un curso, se debe establecer cuál será la carga académica semanal promedio para cada estudiante y los cursos que debe llevar, además de los requisitos y correquisitos de estos.",
     "Concepto_MallaCurricular": "Es el conjunto de asignaturas que un estudiante debe tomar para obtener su título universitario, estas suelen estar divididas en 'ramas' que indican el orden de los cursos a seguir por áreas de conocimiento.",
-    "Concepto_CorteIngreso": "Es el puntaje mínimo requerido para ser admitido en esa carrera, sin embargo, no es un número fijo. Este corte de ingreso suele ser la nota de admisión de la última persona que fue seleccionada para ingresar en esa carrera, por lo que los cortes cambian año a año. Importante: Algunas carreras tienen requisitos especiales como pruebas específicas o de habilidades se RECOMIENDA consultar las normas y procedimientos de ingreso a carrera, correspondiente al año de concurso. Puedes consultar los cortes de carrera en la siguiente dirección: <a href='https://consultasic.ucr.ac.cr/cortespi/' target='_blank'>Cortes UCR</a>.",
+    "Concepto_NotaCorte": "Es el puntaje mínimo requerido para ser admitido en una carrera, sin embargo, no es un número fijo. Este corte de ingreso suele ser la nota de admisión de la última persona que fue seleccionada para ingresar en esa carrera, por lo que los cortes cambian año a año. Importante: Algunas carreras tienen requisitos especiales como pruebas específicas o de habilidades se RECOMIENDA consultar las normas y procedimientos de ingreso a carrera, correspondiente al año de concurso. Puedes consultar los cortes de carrera en la siguiente dirección: <a href='https://consultasic.ucr.ac.cr/cortespi/' target='_blank'>Cortes UCR</a>.",
     "Concepto_PAA": "La Prueba de Aptitud Académica mide habilidades generales de razonamiento mediante ítems que evalúan el nivel de razonamiento de la persona aspirante, combinando conocimientos básicos de los primeros años de secundaria con estrategias como suponer, presuponer, parafrasear, oponer, deducir, reducir, generalizar, verificar, indagar y representar información. La Prueba del 2024 en total posee 50 ítems. Todos son de selección única con 4 opciones de respuesta, de las cuales solo una es correcta. Puedes practicar en línea en el siguiente enlace: <a href='https://www.paa.iip.ucr.ac.cr/practicaenlinea/' target='_blank'>Práctica en Línea PAA</a>.",
-    "Contacto_RecintoParaiso": "Teléfono: 2511-7550, Correo:secretaria.rp@ucr.ac.cr",
-    "Ubicación_RecintoParaiso": "Paraíso de Cartago. Del parque de Paraíso, un kilómetro y medio sobre carretera a Orosi",
-    "Concepto_Correquisito": "Un curso correquisto son cursos que se deben matricular al mismo tiempo que otro curso.",
+    "Ubicacion_RecintoParaiso": "Paraíso de Cartago. Del parque de Paraíso, un kilómetro y medio sobre carretera a Orosi",
+    "Concepto_Correquisito": "Un curso correquisito son cursos que se deben matricular al mismo tiempo que otro curso.",
     "Concepto_RetiroCurso": "Cuando el estudiante decide no llevar un curso previamente matriculado. El retiro no exime de las obligaciones financieras correspondientes, queda RM registrado en el expediente pero no afecta el promedio.",
     "Proceso_RetiroCurso": "Ingresar al sitio web de ematrícula y seleccionar el curso a renunciar en el periodo correspondiente",
     "Concepto_RenunciaCurso": "Cuando el estudiante decide no llevar un curso que matriculó en el periodo de matrícula ordinaria. Este proceso a diferencia del retiro de cursos no tiene implicaciones presupuestarias y no queda registrado en el expediente académico.",
     "Proceso_RenunciaCurso": "No poseer morosidades con la universidad Procedimiento: Ingresar al sitio web de ematrícula y seleccionar el curso a renunciar en el periodo de tiempo estipulado para esto.",
+    "Horario_Biblioteca": "En el recinto de paraíso, la biblioteca está abierta en un horario de 8:00 am a 12:00 md y de 1:00 pm a 5:00 pm",
+
+    
+    #FALTA METER A WIT:
+    "Actividades_HorasEstudiante": "Preparar materiales que sirvan de apoyo en las diferentes actividades universitarias. Colaborar, bajo una supervisión, coordinada del personal académico, docente, en la corrección de tareas, pruebas, trabajos cortos, elaboración de material didáctico y otras actividades conexas. Apoyar al personal académico docente, según se considere pertinente, en actividades relacionadas con las lecciones y los procesos de evaluación de los cursos.",
+    "Actividades_HorasAsistente": "Colaborar, bajo una supervisión coordinada del personal académico docente, en la corrección de tareas, pruebas, trabajos cortos, elaboración de material didáctico y otras actividades semejantes. Colaborar en la organización desarrollo y seguimiento de proyectos en los diferentes ámbitos del quehacer laboratorios o talleres. Colaborar en la organización, desarrollo y seguimiento de proyectos en los diferentes ámbitos del quehacer universitario.",
+    "Actividades_HorasAsistentePosgrado": "Apoyar en la preparación de informes, presentaciones y publicaciones. Colaborar en el diseño y aplicación de diversas metodologías y técnicas de investigación. Colaborar en la organización, ejecución y logística de proyectos en diferentes ámbitos del quehacer universitario. Participar en actividades de un curso con la guía docente.",
+    "Contacto_RecintoParaiso": "Teléfono: 2511-7550, Correo:secretaria.rp@ucr.ac.cr",
     "Servicio_AtenciónPsicologica": "La UCR ofrece atención psicológica gratuita a sus estudiantes a través de la Oficina de Bienestar y Salud. Este servicio brinda apoyo emocional, terapias individuales y talleres sobre salud mental. Puedes solicitar una cita enviando un correo a : PSICOLOGIA.RP@ucr.ac.cr",
     "Servicio_ConsejeriaEstudiantil": "Es un servicio de orientación que ayuda a los estudiantes con problemas académicos, vocacionales y personales. Se ofrece en las distintas facultades y en la Oficina de Orientación.",
     "Servicio_Biblioteca": "La UCR cuenta con múltiples bibliotecas en sus diferentes recintos y sedes proporcionando acceso a libros, revistas, y otros materiales de consulta, tanto físicos como digitales. Además, puedes acceder a las bibliotecas de forma virtual.",
     "Proceso_PrestamoLibros": "Los estudiantes pueden consultar libros a través del catálogo en línea del Sistema de Bibliotecas, Documentación e Información (SIBDI). Si el libro está disponible en la biblioteca, solo deben acercarse y consultar por el número de signatura de este.",
     "Proceso_PrestamoAudiovisuales": "Los materiales audiovisuales pueden ser prestados a través de las bibliotecas que cuentan con este servicio. Se debe presentar la identificación estudiantil y respetar el tiempo máximo de préstamo.",
     "Proceso_ConfiguracionWifi": "Para conectarse al WiFi de la UCR, los estudiantes deben ingresar a la red “eduroam” con su correo institucional (@ucr.ac.cr) y contraseña. También pueden acceder mediante la configuración manual disponible en la página del Centro de Informática.",
-    "Horario_Biblioteca": "En el recinto de paraíso, la biblioteca está abierta en un horario de 8:00 am a 12:00 md y de 1:00 pm a 5:00 pm",
+    
+    
+    "Nota de ingreso":"" #Cuidado con nota corte o nota adimisión
+    
     "por_defecto": "Lo siento, no entiendo tu pregunta. ¿Puedes ser más específico?"
+    
 } 
+
 
 contacts = [
     {"name": "Abigail Velázquez Porras", "email": "abigail.velasquez@ucr.ac.cr", "phone": "2511-7550"},
